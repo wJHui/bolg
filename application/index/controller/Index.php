7c1fadd7c04e529d26fede15aecd88771cc12a1f
company <1245436533@qq.com>
2019-01-26 17:31:45 +0800

    前端更新

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -21,13 +21,15 @@ class Index extends Homebase
 
     public function detail (){
     	if(!$this->request->has('id')){
-
+    		var_dump('none');
     	}
 
     	$id = $this->request->param('id/d');
     	$row = [];
 
-    	$row['detail'] = Article::get($id);
+    	$row['detail'] = Article::hasWhere('articleAata')->where('id', $id)->select();
+
+    	var_dump($row['detail']->articleAata->content);
 
     	$this->assign('row', $row);
     	$this->fetch();
